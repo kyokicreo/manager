d70bae1ba9e4168abd8cd6b57bf4dec5d866244b
kyokicreo <kyokicreo@gmail.com>
2026-07-05 20:08:41 +0600

    pilotBug1

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <cstdint>
 
 #ifdef _WIN32
 #include <windows.h>
+#include <fcntl.h>
+#include <io.h>
 #undef DELETE
 #endif
 
@@ -39,10 +42,85 @@ static bool parseCommandType(const std::string &option, fs::CommandType &outType
     return false;
 }
 
+
+static bool isValidUtf8(const std::string &str)
+{
+    int remainingBytes = 0;
+
+    for (unsigned char c : str)
+    {
+        if (remainingBytes == 0)
+        {
+            if ((c & 0x80) == 0x00)
+            {
+                continue;
+            }
+            else if ((c & 0xE0) == 0xC0)
+            {
+                remainingBytes = 1;
+            }
+            else if ((c & 0xF0) == 0xE0)
+            {
+                remainingBytes = 2;
+            }
+            else if ((c & 0xF8) == 0xF0)
+            {
+                remainingBytes = 3;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if ((c & 0xC0) != 0x80)
+            {
+                return false;
+            }
+            --remainingBytes;
+        }
+    }
+
+    return remainingBytes == 0;
+}
+
+#ifdef _WIN32
+
+static bool readLineUtf8(std::string &outLine)
+{
+    std::wstring wideLine;
+    if (!std::getline(std::wcin, wideLine))
+    {
+        return false;
+    }
+
+    if (wideLine.empty())
+    {
+        outLine.clear();
+        return true;
+    }
+
+    int sizeNeeded = WideCharToMultiByte(
+        CP_UTF8, 0, wideLine.c_str(), static_cast<int>(wideLine.size()),
+        nullptr, 0, nullptr, nullptr);
+
+    outLine.resize(sizeNeeded);
+
+    WideCharToMultiByte(
+        CP_UTF8, 0, wideLine.c_str(), static_cast<int>(wideLine.size()),
+        &outLine[0], sizeNeeded, nullptr, nullptr);
+
+    return true;
+}
+#endif
+
 int main(int argc, char **argv)
 {
 #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    _setmode(_fileno(stdin), _O_U16TEXT);
 #endif
 
     std::string configPath = argc > 1 ? argv[1] : "config/client_config.json";
@@ -62,7 +140,11 @@ int main(int argc, char **argv)
     spdlog::info("Enter the 'HELP' command for detailed information.");
 
     std::string line;
+#ifdef _WIN32
+    while (readLineUtf8(line))
+#else
     while (std::getline(std::cin, line))
+#endif
     {
         std::stringstream ss(line);
         std::string option;
@@ -94,6 +176,12 @@ int main(int argc, char **argv)
         if (path.empty() && option != "LIST" && option != "HISTORY")
         {
             spdlog::warn("This command requires a path argument.");
+            continue;
+        }
+
+        if (!isValidUtf8(path))
+        {
+            spdlog::error("The path contains invalid characters. Make sure your terminal is using UTF-8 encoding.");
             continue;
         }
 
